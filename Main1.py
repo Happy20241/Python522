35ca3fded9e0358f07eaaccbfd97014b6d02d9de
Happy20241 <archer2017@mail.ru>
2025-05-20 01:04:41 +0300

    name

--- Main1.py
+++ Main1.py
@@ -3648,22 +3648,22 @@
 #
 #
 # print(f"Общая площадь всех параллелепипедов: {total_area}")
-
-import sqlite3
-
-
+#
+# import sqlite3
+#
+#
 # con = sqlite3.connect("profile.db")
 # cur = con.cursor()
 #
 # cur.execute(""""""")
 #
 # con.close()
-
-with sqlite3.connect('profile.db') as con:
-    cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PR PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    summ REAL,
-    date BLOB
-    )""")
+#
+# with sqlite3.connect('profile.db') as con:
+#     cur = con.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PR PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     summ REAL,
+#     date BLOB
+#     )""")
